848e8707557991ab23326633131d419fa6709bbf
Cyprien Boree <cyprien.boree@tuta.io>
2025-03-11 11:59:56 +0900

    feat: add docker image for server-pong

--- server-pong/README.md
+++ server-pong/README.md
@@ -1,3 +1,31 @@
 # Server Pong
 
 This server answers to the Server Ping.
+
+## Running with Docker
+
+Build the image:
+
+```console
+docker build -t server-pong .
+```
+
+Run the container:
+
+```console
+docker run -p 8082:8082 -t server-pong
+```
+
+## Running without Docker
+
+Run the server:
+
+```console
+cargo run
+```
+
+In another terminal send the Pong:
+
+```console
+curl http://localhost:8082/pong
+```
--- server-pong/src/server/mod.rs
+++ server-pong/src/server/mod.rs
@@ -1,11 +1,12 @@
 use axum::{Router, routing::get};
 
-// Represents a running server.
+// Represents a running Server.
 pub struct Server {
     http_server: Router,
 }
 
 impl Server {
+    // Creates a new Server instance.
     pub fn new() -> Self {
         return Self {
             http_server: Router::new()
@@ -14,15 +15,23 @@ impl Server {
         };
     }
 
+    // Runs the Server instance.
     pub async fn run(&self) {
         let listener =
-            tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+            tokio::net::TcpListener::bind("0.0.0.0:8082").await.unwrap();
+
         axum::serve(listener, self.http_server.clone())
             .await
             .unwrap();
     }
 }
 
-async fn healthz() {}
+// Handler for the /healthz route.
+async fn healthz() -> String {
+    "OK\n".to_string()
+}
 
-async fn pong() {}
+// Handler for the /pong route.
+async fn pong() -> String {
+    "pong\n".to_string()
+}
