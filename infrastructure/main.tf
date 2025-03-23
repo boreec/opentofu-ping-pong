f446c9bc2b348a35597e2e3d1dfcb955c5926ce9
Cyprien Boree <cyprien.boree@tuta.io>
2025-03-23 14:26:09 +0900

    feat: add helm release for grafana

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -39,3 +39,10 @@ resource "helm_release" "prometheus" {
   repository = "https://prometheus-community.github.io/helm-charts"
   chart      = "prometheus"
 }
+
+resource "helm_release" "grafana" {
+  name       = "grafana"
+  repository = "https://grafana.github.io/helm-charts"
+  chart      = "grafana"
+  depends_on = [helm_release.prometheus]
+}
