acd9829a8e17499691e91dc8126f840db7e14b95
Cyprien Boree <cyprien.boree@tuta.io>
2025-03-30 13:51:03 +0900

    feat: add kube-state-metrics helm-release + fix deployment order

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,39 +1,3 @@
-resource "docker_image" "server_ping_image" {
-  name = "server-ping:latest"
-  build {
-    context    = "${path.module}/../server-ping"
-    dockerfile = "Dockerfile"
-  }
-}
-
-resource "helm_release" "server_ping" {
-  chart = "${path.module}/charts/server-ping"
-  cleanup_on_fail = true
-  depends_on = [ docker_image.server_ping_image ]
-  force_update = true
-  name = "server-ping"
-  recreate_pods = true
-  replace = true
-}
-
-resource "docker_image" "server_pong_image" {
-  name = "server-pong:latest"
-  build {
-    context    = "${path.module}/../server-pong"
-    dockerfile = "Dockerfile"
-  }
-}
-
-resource "helm_release" "server_pong" {
-  chart = "${path.module}/charts/server-pong"
-  cleanup_on_fail = true
-  depends_on = [ docker_image.server_pong_image ]
-  force_update = true
-  name = "server-pong"
-  recreate_pods = true
-  replace = true
-}
-
 resource "helm_release" "prometheus" {
   chart      = "prometheus"
   cleanup_on_fail = true
@@ -41,6 +5,12 @@ resource "helm_release" "prometheus" {
   recreate_pods = true
   repository = "https://prometheus-community.github.io/helm-charts"
   replace = true
+  set = [
+    {
+      name  = "service.type"
+      value = "NodePort"
+    }
+  ]
 }
 
 resource "helm_release" "grafana" {
@@ -67,5 +37,57 @@ resource "helm_release" "grafana" {
     }
   ]
   recreate_pods = true
+  replace = true
+}
+
+resource "docker_image" "server_ping_image" {
+  build {
+    context    = "${path.module}/../server-ping"
+    dockerfile = "Dockerfile"
+  }
+  depends_on = [ helm_release.prometheus ]
+  name = "server-ping:latest"
+}
+
+resource "helm_release" "server_ping" {
+  chart = "${path.module}/charts/server-ping"
+  cleanup_on_fail = true
+  depends_on = [ docker_image.server_ping_image ]
+  force_update = true
+  name = "server-ping"
+  recreate_pods = true
+  replace = true
+}
+
+resource "docker_image" "server_pong_image" {
+  build {
+    context    = "${path.module}/../server-pong"
+    dockerfile = "Dockerfile"
+  }
+  depends_on = [ helm_release.prometheus ]
+  name = "server-pong:latest"
+}
+
+resource "helm_release" "server_pong" {
+  chart = "${path.module}/charts/server-pong"
+  cleanup_on_fail = true
+  depends_on = [ docker_image.server_pong_image ]
+  force_update = true
+  name = "server-pong"
+  recreate_pods = true
+  replace = true
+}
+
+
+resource "helm_release" "kube-state-metrics" {
+  chart = "kube-state-metrics"
+  cleanup_on_fail = true
+  depends_on = [
+    helm_release.server_ping,
+    helm_release.server_pong
+  ]
+  name = "kube-state-metrics"
+  recreate_pods = true
+  repository = "https://prometheus-community.github.io/helm-charts"
   replace = true
 }
