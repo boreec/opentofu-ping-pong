5b2603fcdb1a4d97e4c96614131521afdfd570ba
Cyprien Boree <cyprien.boree@tuta.io>
2025-04-05 23:51:36 +0900

    chore: reduce server-ping to 500kb by using a raw tcp server

--- server-ping/go.mod
+++ server-ping/go.mod
@@ -1,11 +1,3 @@
 module github.com/boreec/opentofu-ping-pong/server-ping
 
 go 1.24.1
-
-require github.com/valyala/fasthttp v1.60.0
-
-require (
-	github.com/andybalholm/brotli v1.1.1 // indirect
-	github.com/klauspost/compress v1.18.0 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-)
--- server-ping/server/server.go
+++ server-ping/server/server.go
@@ -1,36 +1,60 @@
 package server
 
-import "github.com/valyala/fasthttp"
+import (
+	"errors"
+	"io"
+	"net"
+	"strings"
+)
 
+// Server represents the running server.
 type Server struct{}
 
+// NewServer creates a new Server instance.
 func NewServer() *Server {
 	return &Server{}
 }
 
 // Run starts the server.
 func (s *Server) Run() error {
-	mux := func(ctx *fasthttp.RequestCtx) {
-		switch string(ctx.Path()) {
-		case "/healthz":
-			healthz(ctx)
-		case "/ping":
-			ping(ctx)
-		default:
-			ctx.SetStatusCode(fasthttp.StatusNotFound)
-		}
+	listener, err := net.Listen("tcp", ":8081")
+	if err != nil {
+		return err
 	}
+	defer listener.Close()
 
-	return fasthttp.ListenAndServe(":8081", mux)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go handleConnection(conn)
+	}
 }
 
-func ping(ctx *fasthttp.RequestCtx) {
-	if !ctx.IsGet() {
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		if !errors.Is(err, io.EOF) {
+			conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n"))
+		}
 		return
 	}
-	ctx.WriteString("ping")
-}
 
-func healthz(ctx *fasthttp.RequestCtx) {
-	ctx.WriteString("OK")
+	request := string(buf[:n])
+	lines := strings.Split(request, "\r\n")
+	if len(lines) == 0 {
+		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+		return
+	}
+
+	if strings.HasPrefix(lines[0], "GET /healthz ") {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Length: 2\r\n\r\nOK"))
+	} else if strings.HasPrefix(lines[0], "GET /ping ") {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Length: 4\r\n\r\nping"))
+	} else {
+		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
 }
